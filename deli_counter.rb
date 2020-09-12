d3df0bd5ff2ab0709c62d2f43c905b1c9182bd3b
Freedom J Scheyd <joefreedomsch@gmail.com>
2020-09-12 16:55:03 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,7 +19,7 @@ def now_serving(katz_deli)
   if katz_deli.count  == 0
     puts "There is nobody waiting to be served!"
   else
-    katz_deli.shift
-      puts "Currently serving #{}"
+    name = katz_deli.shift
+      puts "Currently serving #{name}."
   end 
 end
