df7511df05ea6108978fc03bc9015bdbc48a410a
Freedom J Scheyd <joefreedomsch@gmail.com>
2020-09-12 16:52:28 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,9 +18,8 @@ end
 def now_serving(katz_deli)
   if katz_deli.count  == 0
     puts "There is nobody waiting to be served!"
-  end
   else
     katz_deli.shift
-      puts 
+      puts
   end
 end
