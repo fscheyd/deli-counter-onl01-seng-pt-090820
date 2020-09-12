4d7500b670f36420009d5459c6048acaf2d55ee8
Freedom J Scheyd <joefreedomsch@gmail.com>
2020-09-12 16:01:34 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,16 @@
-# Write your code here.
+def line(katz_deli)
+  if katz_deli.count == 0
+    puts "The line is currently empty."
+  else 
+    deli_line_sentence = "The line is currently:"
+    katz_deli.each.with_index(1) do |person, i|
+      deli_line_sentence << " #{i}. #{person}"
+    end
+    puts deli_line_sentence
+  end
+end
+
+def take_a_number(katz_deli, name)
+    katz.deli.each.with_index(1) do |i|
+    deli_sentence << "you are number #{i} in line."
+end
